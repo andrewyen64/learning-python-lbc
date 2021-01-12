380196098b6f4968876937be4e508badd4d86926
Andrew <andrewyen64@gmail.com>
2021-01-11 20:15:33 -0800

    Add First Python Program files

---
+++ 1-1stPyProgram/diceroll.py
@@ -0,0 +1,6 @@
+import random
+
+def diceroll():
+    return random.randint(1, 6)
+
+print(diceroll())
---
+++ 1-1stPyProgram/notes.py
@@ -0,0 +1,23 @@
+print(9 + 5, 9 - 5, 9 * 5, 9 / 5)
+#two forward slashes returns whole integer
+print (9 // 5)
+
+# Built-in
+# print(exp)
+
+# area.py -----------------------------
+
+r = 5
+pi = 3.14
+
+area = pi * (r ** 2)
+print(area)
+
+# run 'import math' then 'dir(math)' in terminal to get math lib
+# include 'import math'
+import math
+
+mathArea = math.pi * (r ** 2)
+print(mathArea)
+
+
